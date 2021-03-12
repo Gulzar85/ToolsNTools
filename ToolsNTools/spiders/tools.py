513320d0b21c2d3657c91d4c0a5d71f25dcbbd4e
Gulzar Ahmed <gulzar85@hotmail.com>
2021-03-12 14:33:13 +0500

    "Bugs Fixes"

--- ToolsNTools/spiders/tools.py
+++ ToolsNTools/spiders/tools.py
@@ -16,21 +16,21 @@ class ToolsSpider(scrapy.Spider):
         links = response.css('.hover-mask a:nth-child(1)::attr(href)').getall()
         for link in links:
             link = response.urljoin(link)
-            yield scrapy.Request(link, callback=self.parse_products)
+            yield scrapy.Request(link, callback=self.parse_products, dont_filter=True)
 
     def parse_products(self, response):
         links = response.css('.product-title a::attr(href)').getall()
         for link in links:
             link = response.urljoin(link)
-            yield scrapy.Request(link, callback=self.parse_product)
+            yield scrapy.Request(link, callback=self.parse_product, dont_filter=True)
     
     def parse_product(self, response):
         yield {
-            'Product_Name' : sh.cleanup(response.css('.product_title.entry-title::text').get()),
-            'Product_Price' : float(response.css('.basel-scroll-content ins bdi::text').get()),
-            'Availability' : response.css('.in-stock::text').get(),
-            'SKU' : response.css('.sku::text').get(),
-            'Category' : response.css('.posted_in a::text').get(),
-            'URL' : response.url,
-            'image_urls' : [response.css('.woocommerce-product-gallery__image a::attr(href)').get()],
-        }
+            'Product_Name': sh.cleanup(response.css('.product_title.entry-title::text').get()),
+            'Product_Price': float(response.css('.basel-scroll-content ins bdi::text').get()),
+            'Availability': response.css('p:contains("stock")::text').get(),
+            'SKU': response.css('.sku::text').get(),
+            'Category': response.css('.posted_in a::text').get(),
+            'URL': response.url,
+            'image_urls': [response.css('.woocommerce-product-gallery__image a::attr(href)').get()],
+            }
